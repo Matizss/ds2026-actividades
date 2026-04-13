a1d7946f2dfd63db0cf48d60b787c7d0ed3edf5f
matizss <matiasfer2005@gmail.com>
2026-04-13 11:54:32 -0300

    Condicionales

---
+++ ejercicios/c03-javascript/ej2-condicionales/app.js
@@ -0,0 +1,33 @@
+// Ejercicio 2 — Condicionales
+// ● Crear una función clasificarNota(nota) que reciba un número y
+// retorne:
+// ○ "Desaprobado" si es menor a 4
+// ○ "Aprobado" si es entre 4 y 7
+// ○ "Promocionado" si es 8 o más
+// ● Crear una función diaDeLaSemana(numero) con switch que retorne
+// el nombre del día (1=Lunes...7=Domingo). Si es 6 o 7 agregar
+// "(fin de semana)". Si no es 1-7, retornar "Día inválido".
+// ● Probar ambas funciones con console.log usando distintos
+// valores.
+
+
+const clasificarNota = (nota) => {
+    if (nota < 4) return console.log("Desaprobado");
+    if (nota >= 4 && nota <8) return console.log("Aprobado");
+    if (nota >= 8) return console.log("Promocionado");
+}
+
+
+const diaDeLaSemana = (numero) => {
+    switch (numero) {
+        case 1: console.log("Lunes"); break;
+        case 2: console.log("Martes"); break;
+        case 3: console.log("Miércoles"); break;
+        case 4: console.log("Jueves"); break;
+        case 5: console.log("Viernes"); break;
+        case 6: console.log("Sabado"); break;
+        case 7: console.log("Domingo"); break;
+
+        default: console.log("Ingrese un numero entre el 1 y el 7." );
+    }
+}
