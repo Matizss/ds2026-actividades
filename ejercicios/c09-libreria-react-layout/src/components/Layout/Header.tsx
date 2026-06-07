1b99bc600ce280b55c69d79b303b935c61bd9dfd
matizss <matiasfer2005@gmail.com>
2026-06-07 18:18:29 -0300

    feat: agregar Layout

--- ejercicios/c09-libreria-react-layout/src/components/Layout/Header.tsx
+++ ejercicios/c09-libreria-react-layout/src/components/Layout/Header.tsx
@@ -1,31 +1,34 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg py-3 sticky-top bg-primary-subtle" data-bs-theme="dark">
-      <div className="container-fluid">
-        <a className="navbar-brand d-flex align-items-center gap-2" href="#">
-          <img src="/img/logo.png" alt="Logo" width="35" height="29" />
+    <Navbar
+      expand="lg"
+      className="py-3 sticky-top bg-primary-subtle"
+      data-bs-theme="dark"
+    >
+      <Container fluid>
+        <Navbar.Brand
+          href="#"
+          className="d-flex align-items-center gap-2"
+        >
+          <img src="/img/logo.png" width="35" height="29" />
           Mi librería
-        </a>
+        </Navbar.Brand>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <Navbar.Toggle aria-controls="navbarText" />
 
-        <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fs-6">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="catalogo.html">Catálogo</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="contacto.html">Contacto</a>
-                </li>  
-            </ul>
-        </div>
-      </div>
-    </nav>
+        <Navbar.Collapse id="navbarText">
+          <Nav className="ms-auto mb-2 mb-lg-0 fs-6">
+            <Nav.Link href="#" active>
+              Inicio
+            </Nav.Link>
+            <Nav.Link href="#">Catálogo</Nav.Link>
+            <Nav.Link href="#">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
